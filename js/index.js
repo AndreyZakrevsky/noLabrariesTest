27a0e368dbf8290fd10a1e87bb9b26ed9bfc3451
Andrey Zakrevsky <andrei.zakrevskii@dott.pro>
2019-11-21 10:53:42 +0200

    Test is compited

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
+    // Fake state
     let m = 7;
     let n = 7;
     let parentPositionLeftSum = null;
@@ -6,78 +7,119 @@ document.addEventListener('DOMContentLoaded', () => {
     let hoverValue = null;
     let deleteRowProccess = false;
     let sumOfRow = null;
+    let indexRow = null;
 
-
+// First data init process
     let data = ((m, n) => {
         let res = [];
         let itemRow = 0;
         for (let i = 0; i < m * n; i++) {
-            if(i % n === 0){ itemRow++;}
+            if (i % n === 0) {
+                itemRow++;
+            }
             res.push({
                 column: Math.floor(i % n) + 1,
                 row: itemRow,
                 id: i * Math.floor(Math.random() * (2000 - 10)) + 10,
-               // amount: Math.floor(Math.random() * (999 - 100)) + 100
-                amount: 2
+                amount: Math.floor(Math.random() * (999 - 100)) + 100
             });
         }
-
         return res;
     })(m, n);
 
+// Calculate average values of each column (array)
     let calcAvarages = () => {
         data.map((item) => {
             averages[item.column - 1] = averages[item.column - 1] ? averages[item.column - 1] + item.amount : item.amount;
         });
-
         averages = averages.map((i) => (i / m).toFixed(1));
     };
 
-    let addColl = (tr, el) => {
+// Click to element of table for call incrementing element or delete row
+    let addToItem = async (event) => {
+
+        let td = event.target;
+
+        if (!td) return;
+
+        if (deleteRowProccess) {
+            let id = event.target.getAttribute("data-id");
+            let row = 0;
+            m--;
+            data.map((item) => {
+                if (+item.id === +id) {
+                    row = item.row;
+                }
+            });
+            data = data.filter((item) => item.row !== row);
+            data = data.map((item) => {
+                if (item.row > row) {
+                    return {...item, row: item.row - 1}
+                } else {
+                    return item;
+                }
+            });
+            await clearTable();
+            renderTable(m, n);
+
+        } else {
+            data = data.map((item) => {
+                if (+item.amount === +td.innerHTML && +item.id === +td.dataset.id) {
+                    item.amount = item.amount + 1;
+                }
+                return item;
+            });
+            await clearTable();
+            renderTable(m, n);
+        }
+
+    };
+
+// Hover approximate values of elements
+    let approximateValues = async (event) => {
+        if (+event.target.innerHTML !== +hoverValue) {
+            hoverValue = +event.target.innerHTML;
+            clearTable();
+            renderTable(m, n);
+        }
+    };
+
+// Add columns to table
+    let addColl = (tr, el, i) => {
         let td = document.createElement('td');
 
-        if(sumOfRow){
-            td.innerHTML = sumOfRow;
-        }else{
+        if (sumOfRow && indexRow) {
+            if( +indexRow === +el.row){
+                td.innerHTML =   `${Math.floor( (+el.amount * 100) / +sumOfRow )} %`;
+                td.setAttribute('class', 'persent')
+            }else{
+                td.innerHTML = el.amount;
+            }
+        } else {
             td.innerHTML = el.amount;
         }
 
-
-        if (hoverValue && !deleteRowProccess) {
+        if (hoverValue && !deleteRowProccess && !indexRow) {
             let per = (hoverValue / 100) * 20;
-
             if (el.amount >= (hoverValue - per) && el.amount <= (hoverValue + per)) {
-                td.setAttribute('class', 'active')
+                td.setAttribute('class', 'active');
             }
-
         }
         td.setAttribute('data-id', el.id);
 
-        td.addEventListener('click',addToItem , false);
+        td.addEventListener('click', addToItem, false);
 
         td.addEventListener('mousemove', approximateValues, false);
 
         tr.appendChild(td);
     };
 
-    let approximateValues = async(event)=>{
-
-        if (+event.target.innerHTML !== +hoverValue) {
-          //  document.documentElement.scrollTop = document.body.scrollTop = event.target.getBoundingClientRect().top;
-            hoverValue = +event.target.innerHTML;
-
-            clearTable();
-
-            renderTable(m, n);
-        }
-
-    };
-
-    let addRow = async (tbl, col, step) => {
+// Add rows to table
+    let addRow = async (tbl, col, step, i) => {
         let tr = document.createElement('tr');
         let rowSum = 0;
         for (let i = 0; i < col; i++) {
-            await addColl(tr, data[step]);
+            await addColl(tr, data[step], i + 1);
             rowSum += data[step].amount;
             step++;
         }
@@ -91,41 +133,58 @@ document.addEventListener('DOMContentLoaded', () => {
         span.style.position = 'absolute';
         span.style.left = parentPositionLeftSum + (48 * col) + 'px';
         span.style.top = parentPositionTopSum + 15 + 'px';
+        span.setAttribute("data-index", i + 1);
 
-        span.addEventListener('mousemove', (item)=>{
-            sumOfRow = item.target.innerText;
-            clearTable();
-            renderTable(m, n);
-        });
+        span.addEventListener('mousemove', function (event) {
+            sumOfRow = null;
+            indexRow = null;
+            if (!sumOfRow && !deleteRowProccess) {
+                sumOfRow = event.target.innerText;
+                indexRow = event.target.getAttribute("data-index");
+                let timer = setTimeout(() => {
+                    sumOfRow = null;
+                    indexRow = null;
+                    clearTable();
+                    renderTable(m, n);
+                    clearTimeout(timer);
+                }, 3000)
+                clearTable();
+                renderTable(m, n);
+            }
+
+        }, false);
+
 
         block.append(span);
 
         return step;
     };
 
+// Render  table
     let renderTable = async (row = 9, col = 9) => {
 
         let block = document.getElementById('block');
+
         let tbl = document.createElement('table');
-        if(deleteRowProccess){
+
+        if (deleteRowProccess) {
             tbl.setAttribute("class", "table rounded delete_row_mode");
-        }else{
+        } else {
             tbl.setAttribute("class", "table rounded");
         }
         tbl.setAttribute("id", "table");
         block.append(tbl);
         let step = 0;
         for (let i = 0; i < row; i++) {
-            step = await addRow(tbl, col, step)
+            step = await addRow(tbl, col, step, i)
         }
         await calcAvarages();
         let x = document.getElementById("table").rows.length;
         let lastRow = document.getElementById("table").rows[x - 1].cells;
         let i = 0;
-        for (let td  of lastRow) {
+        for (let td of lastRow) {
             let topTd = td.getBoundingClientRect().top;
             let leftTd = td.getBoundingClientRect().left;
-
             let span = document.createElement('span');
             span.innerText = averages[i];
             span.style.position = 'absolute';
@@ -133,7 +192,8 @@ document.addEventListener('DOMContentLoaded', () => {
             span.style.left = leftTd + 10 + 'px';
             block.append(span);
             i++;
-        };
+        }
+        ;
 
         tbl.onmouseleave = () => {
             hoverValue = null;
@@ -142,44 +202,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     };
 
-    let addToItem = async (event) => {
-
-        let td = event.target;
-
-        if (!td) return;
-
-        if(deleteRowProccess){
-
-            let id = event.target.getAttribute("data-id");
-            let row = 0;
-            m--;
-            data.map((item)=>{
-                if(+item.id === +id){
-                    row = item.row;
-                 }
-            });
-            data = data.filter( (item)=> item.row !== row );
-            data = data.map( (item)=> {
-                if(item.row > row) {
-                    return {...item, row: item.row - 1}
-                }else{ return item; }
-            });
-            await clearTable();
-            renderTable(m, n);
-
-        }else{
-            data = data.map((item) => {
-                if (+item.amount === +td.innerHTML && +item.id === +td.dataset.id) {
-                    item.amount = item.amount + 1;
-                }
-                return item;
-            });
-            await clearTable();
-            renderTable(m, n);
-        }
-
-    };
-
+// Clear table (before render)
     let clearTable = () => {
         let table = document.getElementById('table');
         for (var i = 0, row; row = table.rows[i]; i++) {
@@ -197,50 +220,52 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     };
 
+
+// For delete row
+    document.getElementById('delete-btn').addEventListener('click', (() => {
+        alert('Press in row which you want delete ( 10 seconds ) ');
+        deleteRowProccess = true;
+        clearTable();
+        renderTable(m, n);
+        setTimeout(() => {
+            deleteRowProccess = false;
+            clearTable();
+            renderTable(m, n);
+        }, 10000);
+    }), false);
+
+// For add row
+    document.getElementById('add-btn').addEventListener('click', ((event) => {
+        document.querySelector('.page').style.minHeight = event.target.getBoundingClientRect().top + 200;
+        // console.log(event.target.getBoundingClientRect().top);
+        m++;
+        deleteRowProccess = false;
+        let tempArr = [];
+        let lastRow = 0;
+        let column = 1;
+        for (let i = 0; i < m * n; i++) {
+            if (typeof data[i] === 'undefined') {
+                // does not exist
+                tempArr[i] = {
+                    column: column,
+                    row: lastRow + 1,
+                    id: i * Math.floor(Math.random() * (2000 - 10)) + 10,
+                    amount: Math.floor(Math.random() * (999 - 100)) + 100
+                };
+                column++;
+            } else {
+                // does exist
+                tempArr[i] = data[i];
+                lastRow = data[i].row;
+            }
+
+        }
+
+        data = tempArr;
+        clearTable();
+        renderTable(m, n);
+
+    }), false);
+
     renderTable(m, n);
-
-
-   document.getElementById('delete-btn').addEventListener('click', ( ()=>{
-       alert('Press in row which you want delete ( 10 seconds ) ');
-       deleteRowProccess = true;
-       clearTable();
-       renderTable(m, n);
-       setTimeout( ()=>{
-           deleteRowProccess = false;
-           clearTable();
-           renderTable(m, n);
-       } , 10000);
-   } ) , false);
-   document.getElementById('add-btn').addEventListener('click', ( (event)=>{
-       document.querySelector('.page').style.minHeight = event.target.getBoundingClientRect().top + 200;
-       // console.log(event.target.getBoundingClientRect().top);
-       m++;
-       deleteRowProccess = false;
-       let tempArr = [];
-       let lastRow = 0;
-       let column = 1;
-       for (let i = 0; i < m * n; i++) {
-           if(typeof data[i] === 'undefined') {
-               // does not exist
-               tempArr[i] = {
-                   column: column,
-                   row: lastRow + 1,
-                   id: i * Math.floor(Math.random() * (2000 - 10)) + 10,
-                   amount: Math.floor(Math.random() * (999 - 100)) + 100
-               };
-               column++;
-           }
-           else {
-               // does exist
-               tempArr[i] = data[i];
-               lastRow = data[i].row;
-           }
-
-       }
-
-       data = tempArr;
-       clearTable();
-       renderTable(m, n);
-
-   } ) , false);
 });
